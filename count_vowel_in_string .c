d998373531a0c723779e52fe2cfd2f2580ed340e
Rahul Raj <49182758+raj2029rahul@users.noreply.github.com>
2023-02-15 12:29:40 +0530

    Update and rename count vowel in string .c to count_vowel_in_string .c

--- count_vowel_in_string .c
+++ count_vowel_in_string .c
@@ -1,10 +1,11 @@
 #include<stdio.h>
+#include<string.h>
 main()
 {
     char s[100];
     int i,count=0;
     printf("enter sentence for vowel count\n");
-    scanf("%s",&s);
+    fgets(s,50,stdin);
     for(i=0;s[i]!='\0';i++)
    {
      if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u')

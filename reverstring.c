c6081231347cc94dafe3254722d0d88b1f58563a
Rahul Raj <49182758+raj2029rahul@users.noreply.github.com>
2023-02-15 12:37:23 +0530

    Update reverstring.c

--- reverstring.c
+++ reverstring.c
@@ -1,10 +1,17 @@
 #include<stdio.h>
 #include<string.h>
-main()
+int main()
 {
+    int temp,len;
     char s[100];
     printf("enter string\n");
-    gets(s);
-    strrev(s);
-    printf("reverse of a string is\n %s\n",s);
+    fgets(s,50,stdin);
+    len=strlen(s);
+    for(int i=0;i<len/2;i++){
+        temp=s[i];
+        s[i]=s[len-i-1];
+        s[len-i-1]=temp;
+    }
+    printf("Reverse of a string is:%s",s);
+    return 0;
 }
